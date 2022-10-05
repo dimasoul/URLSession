0968f49e1ca9612046194ee4394620ef9925834c
mitya-toretto <dimahryasheff@yandex.ru>
2022-10-05 15:11:22 +0300

    URL

--- URLSession/ViewController.swift
+++ URLSession/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var jokeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        makeRequest()
     }
-
+    
+    private func makeRequest() {
+        guard let url = URL(string: "https://v2.jokeapi.dev/joke/Any?safe-mode") else {return}
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            do {
+                let joke = try JSONDecoder().decode(Joke.self, from: data)
+                print(joke.setup)
+                DispatchQueue.main.async {
+                    self.jokeLabel.text = joke.setup
+                }
+                
+            } catch {
+                print(error.localizedDescription)
+            }
+            
+        }.resume()
+    }
 
 }
 
